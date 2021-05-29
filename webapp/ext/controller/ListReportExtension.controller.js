ab1c51eb7b1050e26c993cccad735305420ee85a
Prashanth C M <prashupachi.cm@gmail.com>
2021-05-29 05:34:43 +0000

    Add alist report

---
+++ webapp/ext/controller/ListReportExtension.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+
+], function () {
+    'use strict';
+
+    return sap.ui.controller("fiorielements.ext.controller.ListReportExtension", {
+        onInit: function (oEvent) {
+            alert('onInit');
+        }
+    });
+});
---
+++ webapp/ext/controller/ObjectPageCon.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+"sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function (Filter, FilterOperator) {
+    'use strict';
+
+    return sap.ui.controller("fiorielements.ext.controller.ObjectPageCon", {
+        onInit: function (oEvent) {
+            alert('onInit');
+        },
+
+        onBeforerebindTable: function (oEvent){
+            debugger
+            var oBindingParameter = oEvent.getParameter("bindingParams");
+            var aFilters = [
+                new Filter("CategoryID", FilterOperator.EQ, 1)
+            ];
+            //oBindingParameter.filters = oBindingParameter.filters.concat(aFilters);
+        }
+    });
+});
